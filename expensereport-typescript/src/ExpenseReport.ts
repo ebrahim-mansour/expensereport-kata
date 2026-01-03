d7bde8be949a2b567586b939572d5931c9e6ae33
Ebrahim Mansour <ebrahimmansour2001@gmail.com>
2026-01-03 19:21:45 +0200

    refactor: replace string literals with enum values for meal types to enhance type safety

--- expensereport-typescript/src/ExpenseReport.ts
+++ expensereport-typescript/src/ExpenseReport.ts
@@ -8,12 +8,17 @@ const printHelloWorld = (): void => {
 
 type ExpenseType = Dinner | Breakfast | CarRental
 
+enum Meals {
+  Dinner = "dinner",
+  Breakfast = "breakfast"
+}
+
 type Dinner = {
-  key: "dinner",
+  key: Meals.Dinner,
   name: "Dinner"
 }
 type Breakfast = {
-  key: "breakfast"
+  key: Meals.Breakfast
   name: "Breakfast"
 }
 type CarRental = {
@@ -36,14 +41,14 @@ class Expense {
   }
 
   get mealAndOverExpensesThreshold(): string {
-    const isDinnerOver = this.key == "dinner" && this.amount > DINNER_THRESHOLD;
-    const isBreakFastOver = this.key == "breakfast" && this.amount > BREAKFAST_THRESHOLD;
+    const isDinnerOver = this.key == Meals.Dinner && this.amount > DINNER_THRESHOLD;
+    const isBreakFastOver = this.key == Meals.Breakfast && this.amount > BREAKFAST_THRESHOLD;
     return isDinnerOver || isBreakFastOver ? "X" : " ";
   }
 }
 
 function calculateMealExpenses(expense: Expense, mealExpenses: number) {
-  const isMeal = expense.key == "dinner" || expense.key == "breakfast";
+  const isMeal = expense.key == Meals.Dinner || expense.key == Meals.Breakfast;
   if (isMeal) {
     mealExpenses += expense.amount;
   }
